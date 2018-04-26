7aafa08ba6a08d6017d23ba25a52c857174c61fd
贺宇欣 <heyuxinerya@sohu.com>
2018-04-26 23:30:37 +0800

    first commit

---
+++ js/demo.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(function(){
+    $(".btn1").click(function(){
+	  $(".box").show();
+	})	
+	$(".btn2").click(function(){
+	  $(".box").hide();
+	})
+	$(".btn3").click(function(){
+	  $(".box").taggle();
+	})
+})
+alert(123);
